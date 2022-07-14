00bce8643bf42cfddf607485e3896db6c6cbaacf
ATinker <1061859175@qq.com>
2022-07-14 23:30:15 +0800

    feat(twr_dev):构建了抽象类dtkOjbect作为物体的基类，并封装了dtkMesh，继承自dtkObject，实现了物体与模拟场景的解耦

--- demo/FEMsimulation/dtkFemSimulation.cpp
+++ demo/FEMsimulation/dtkFemSimulation.cpp
@@ -55,62 +55,10 @@ inline int mesh(int i, int j) {
 
 dtkFemSimulation::dtkFemSimulation(unsigned int width, unsigned int height)
 	: dtkScene(width, height),
-	points(n_node), pre_points(n_node), points_v(n_node), points_force(n_node), B(n_fem_element),
-	MeshTable(n_fem_element, std::vector<int>(3, 0)), sphere(dtk::dtkGraphicsKernel::Point2(0.5, 0.25), radius),
-	total_energy(0), pre_total_energy(0), spherecenter(0.5, 0.25)
+	sphere(dtk::dtkGraphicsKernel::Point2(0.5, 0.25), radius),
+	spherecenter(0.5, 0.25), rectangle(n_node_x, n_node_y)
 {
 
-}
-
-Matrix2f dtkFemSimulation::compute_D(int i) {
-	int a = MeshTable[i][0];
-	int b = MeshTable[i][1];
-	int c = MeshTable[i][2];
-
-	Matrix2f ans;
-	ans(0, 0) = points[a][0] - points[c][0];
-	ans(0, 1) = points[b][0] - points[c][0];
-	ans(1, 0) = points[a][1] - points[c][1];
-	ans(1, 1) = points[b][1] - points[c][1];
-	return ans;
-}
-
-
-void dtkFemSimulation::compute_B() {
-	for (int i = 0; i < n_fem_element; ++i) {
-		this->B[i] = compute_D(i).inverse();
-		//cout << setw(6) << B[i] << endl;
-	}
-}
-
-Matrix2f dtkFemSimulation::compute_P(int i) {
-	Matrix2f D = compute_D(i);
-	Matrix2f F = D * B[i];
-
-	Matrix2f F_T = F.transpose().inverse();
-
-	float J = max(0.5f, F.determinant()); /**< 形变率 */
-
-	return Lame_parameter_1 * (F - F_T) + Lame_parameter_2 * log(J) * F_T;
-	//Matrix2f ans = D * this->B[i];
-}
-
-void dtkFemSimulation::compute_total_energy() {
-
-	//this->total_energy = 0.0f;
-	for (int i = 0; i < n_fem_element; ++i) {
-		Matrix2f D = compute_D(i);
-		Matrix2f F = D * B[i];
-
-		//NeoHooken
-		float I1 = (F * F.transpose()).trace();
-		float J = max(0.2f, (float)F.determinant()); /**< 形变率 */
-
-		//cout << J << endl;
-
-		float element_energy_density = 0.5 * Lame_parameter_1 * (I1 - dim) - Lame_parameter_1 * log(J) + 0.5 * Lame_parameter_2 * log(J) * log(J);
-		this->total_energy += element_energy_density * element_v;
-	}
 }
 
 dtkFemSimulation::~dtkFemSimulation()
@@ -120,6 +68,7 @@ dtkFemSimulation::~dtkFemSimulation()
 void dtkFemSimulation::Init()
 {
 	dtkScene::Init();
+	rectangle.Init();
 	//TODO: load shaders
 
 	//TODO: configure shaders
@@ -131,64 +80,10 @@ void dtkFemSimulation::Init()
 	//TODO: configure Scene objects
 
 	//build mesh
-	for (int i = 0; i < n_node_x; ++i) {
-		for (int j = 0; j < n_node_y; ++j) {
-			int idx = mesh(i, j);
-			//this->points[idx][0] = -14 + i * deltax * 0.5;
-			//this->points[idx][1] = 8 + j * deltax * 0.5 + i * deltax * 0.05;
-
-			this->points[idx][0] = 0.1f + i * deltax * 0.5f;
-			this->points[idx][1] = 0.5f + j * deltax * 0.5f + i * deltax * 0.1f;
-			this->points_v[idx][0] = 0.0f;
-			this->points_v[idx][1] = -1.0f;
-		}
-	}
-
-	//this->pre_points = points;
-
-	for (int i = 0; i < n_node_x - 1; ++i) {
-		for (int j = 0; j < n_node_y - 1; ++j) {
-			//element id
-			int eidx = (i * (n_node_y - 1) + j) * 2;
-			this->MeshTable[eidx][0] = mesh(i, j);
-			this->MeshTable[eidx][1] = mesh(i + 1, j);
-			this->MeshTable[eidx][2] = mesh(i, j + 1);
-
-			eidx = (i * (n_node_y - 1) + j) * 2 + 1;
-			this->MeshTable[eidx][0] = mesh(i, j + 1);
-			this->MeshTable[eidx][1] = mesh(i + 1, j + 1);
-			this->MeshTable[eidx][2] = mesh(i + 1, j);
-		}
-	}
-
-	compute_B();
 
 	//TODO: audio
 }
-
-void dtkFemSimulation::compute_force() {
-	for (int i = 0; i < n_node; ++i) {
-		this->points_force[i] = Vector2f(0.0f, -10.0f * node_mass);
-	}
-
-	for (int i = 0; i < n_fem_element; ++i) {
-
-		Matrix2f P = compute_P(i);
-		Matrix2f H = -element_v * (P * (this->B[i].transpose()));
-
-		Vector2f h1 = Vector2f(H(0, 0), H(1, 0));
-		Vector2f h2 = Vector2f(H(0, 1), H(1, 1));
-
-		int a = this->MeshTable[i][0];
-		int b = this->MeshTable[i][1];
-		int c = this->MeshTable[i][2];
-
-		this->points_force[a] += h1;
-		this->points_force[b] += h2;
-		this->points_force[c] += -(h1 + h2);
-	}
-}
-
+/*
 void dtkFemSimulation::Update(float dt)
 {
 	//TODO: update objects
@@ -197,7 +92,7 @@ void dtkFemSimulation::Update(float dt)
 
 		// 迭代多轮, 防止穿透
 		for (int i = 0; i < iterate_time; ++i) {
-			//this->pre_total_energy = total_energy; 
+			//this->pre_total_energy = total_energy;
 			compute_total_energy();
 			DoCollisions();
 			compute_force();
@@ -205,7 +100,7 @@ void dtkFemSimulation::Update(float dt)
 			//cout << total_energy << endl;
 
 			//float deltaU = this->total_energy - this->pre_total_energy;
-			//deltaU = abs(deltaU) < 1e-9 ? 0 : deltaU; 
+			//deltaU = abs(deltaU) < 1e-9 ? 0 : deltaU;
 			for (int j = 0; j < n_node; ++j) {
 				// update points
 
@@ -233,8 +128,31 @@ void dtkFemSimulation::Update(float dt)
 			// this->pre_total_energy = this->total_energy;
 		}
 	}
-}
+}*/
 
+void dtkFemSimulation::Update(float dt)
+{
+	//TODO: update objects
+	//TODO: check for object collisions
+	if (this->State == SCENE_ACTIVE) {
+
+		// 迭代多轮, 防止穿透
+		for (int i = 0; i < iterate_time; ++i) {
+			//this->pre_total_energy = total_energy; 
+
+			rectangle.compute_total_energy();
+			DoCollisions();
+			rectangle.compute_force();
+
+			for (int j = 0; j < n_node; ++j) {
+				rectangle.points_v_[j] = (rectangle.points_v_[j] + (rectangle.points_force_[j] / rectangle.node_mass_) * deltat) * exp(deltat * -3);
+
+				rectangle.points_[j] += deltat * rectangle.points_v_[j];
+			}
+
+		}
+	}
+}
 
 void dtkFemSimulation::ProcessInput(float dt)
 {
@@ -242,7 +160,6 @@ void dtkFemSimulation::ProcessInput(float dt)
 	//TODO: process input(keys)
 
 }
-
 
 void dtkFemSimulation::Render()
 {
@@ -268,12 +185,12 @@ void dtkFemSimulation::Render()
 	glBegin(GL_LINES);
 	for (int i = 0; i < n_fem_element; ++i) {
 		for (int j = 0; j < 3; ++j) {
-			int a = this->MeshTable[i][j];
-			int b = this->MeshTable[i][(j + 1) % 3];
+			int a = rectangle.mesh_table_[i][j];
+			int b = rectangle.mesh_table_[i][(j + 1) % 3];
 
 			//draw line from a to b;
-			glVertex2f(this->points[a][0], this->points[a][1]);
-			glVertex2f(this->points[b][0], this->points[b][1]);
+			glVertex2f(rectangle.points_[a][0], rectangle.points_[a][1]);
+			glVertex2f(rectangle.points_[b][0], rectangle.points_[b][1]);
 		}
 	}
 	glEnd();
@@ -293,36 +210,36 @@ void dtkFemSimulation::DoCollisions()
 		for (int i = 0; i < n_node; ++i) {
 			//# Collide with sphere
 
-			Vector2f dis = this->points[i] - center;
+			Vector2f dis = rectangle.points_[i] - center;
 			if ((float)(dis.dot(dis)) < radius * radius)
 			{
 				Vector2f normal = dis.normalized();
 
-				this->points[i] = center + radius * normal;
-				this->points_v[i] -= (this->points_v[i].dot(normal)) * normal;
+				rectangle.points_[i] = center + radius * normal;
+				rectangle.points_v_[i] -= (rectangle.points_v_[i].dot(normal)) * normal;
 			}
 
 
 			// Collide with ground
 
-			if (this->points[i][1] < 0.2f) {
-				this->points[i][1] = 0.2f;
-				this->points_v[i][1] = 0.0f;
+			if (rectangle.points_[i][1] < 0.2f) {
+				rectangle.points_[i][1] = 0.2f;
+				rectangle.points_v_[i][1] = 0.0f;
 			}
 
-			if (this->points[i][1] > 0.9f) {
-				this->points[i][1] = 0.9f;
-				this->points_v[i][1] = 0.0f;
+			if (rectangle.points_[i][1] > 0.9f) {
+				rectangle.points_[i][1] = 0.9f;
+				rectangle.points_v_[i][1] = 0.0f;
 			}
 
-			if (this->points[i][0] < 0.0f) {
-				this->points[i][0] = 0.0f;
-				this->points_v[i][0] = 0.0f;
+			if (rectangle.points_[i][0] < 0.0f) {
+				rectangle.points_[i][0] = 0.0f;
+				rectangle.points_v_[i][0] = 0.0f;
 			}
 
-			if (this->points[i][0] > 1.0f) {
-				this->points[i][0] = 1.0f;
-				this->points_v[i][0] = 0.0f;
+			if (rectangle.points_[i][0] > 1.0f) {
+				rectangle.points_[i][0] = 1.0f;
+				rectangle.points_v_[i][0] = 0.0f;
 			}
 		}
 
@@ -332,8 +249,4 @@ void dtkFemSimulation::DoCollisions()
 void dtkFemSimulation::moveBall(int x, int y) {
 
 	//this->spherecenter = Vector2f((x) * 1.0 / 800 , (600 - y) * 1.0 / 600 );
-}
-
-float dtkFemSimulation::getEnergy() {
-	return this->total_energy;
 }
