f758066171c3eb570540e7af58bfa45c51716473
For-Chance <chancewithchance@outlook.com>
2024-08-22 08:37:59 +0800

    ci: try to fix github ci

--- cmake/3rd.cmake
+++ cmake/3rd.cmake
@@ -46,6 +46,9 @@ endif ()
 include(${CPM_DOWNLOAD_LOCATION})
 # -------- get_cpm.cmake --------
 
+# Ensure GLEW_USE_STATIC_LIBS is set consistently
+set(GLEW_USE_STATIC_LIBS ON CACHE BOOL "Use static GLEW libraries")
+
 # https://github.com/google/googletest
 CPMAddPackage(
         NAME googletest
